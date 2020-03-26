32bfdcfbfe749c8697a86e43f0529002ad49a28d
lanbra <laura.brandstetter@web.de>
2020-03-26 16:09:00 +0100

    console

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,20 @@
 // alert("Hello World");
+// let message = "Hello World";
+// alert(message);
+// message = "Hallo Welt";
+
+const LINK_COLOR = "ff0000";
+console.log("Link bitte in der Farbe ", LINK_COLOR);
+
+let highscore = 520233;
+console.log(highscore)
+
+let firstname = "Laura";
+let lastname = 'Brandstetter';
+console.log("Name: ", firstname, lastname);
+
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log(fullname);
+
+let template = `Dein Highscore ist ${highscore} Punkte`;
+console.log (template)
