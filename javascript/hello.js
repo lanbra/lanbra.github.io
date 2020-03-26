b313a0fede8d21e320f654a09b1d7c804a47a688
lanbra <laura.brandstetter@web.de>
2020-03-26 17:51:41 +0100

    console null 3

--- javascript/hello.js
+++ javascript/hello.js
@@ -110,7 +110,7 @@ users.forEach(user => {
     console.log(`${user.firstname} ist oder wird heuer ${calcAge(user.birthyear)} Jahre alt`);
 });
 
-let firstParagraph = document.querySelector("#pFirst")
+let firstParagraph = document.querySelector("#pFirst");
 console.log(firstParagraph);
 // first paragraph remove
 firstParagraph.innerHTML="Test";
