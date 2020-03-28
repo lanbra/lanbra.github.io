72f48122ae906c1351745b1977003d03c16742e5
lanbra <laura.brandstetter@web.de>
2020-03-28 16:12:47 +0100

    london mag nicht

--- nz/main.js
+++ nz/main.js
@@ -9,8 +9,9 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap); */
 
+let Karte = document.querySelector("#map");
 
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+let mymap = L.map(Karte).setView([51.505, -0.09], 13);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
