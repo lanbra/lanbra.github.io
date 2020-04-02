2795c572708878ef3cfb0cbaac53100f0c5622e2
lanbra <laura.brandstetter@web.de>
2020-04-02 18:03:04 +0200

    glump

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ let map = L.map("map", {
     ]
 });
 
+let circleGroup = l.featureGroup().addTo(map);
+
+
 L.control.layers({
     "OpenTopoMap": startLayer,
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
@@ -15,6 +18,8 @@ L.control.layers({
     "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
     "Stamen.Toner": L.tileLayer.provider("Stamen.Toner")
     
+}, {
+    "Thematische Darstellung":circleGroup
 }).addTo(map);
 
 // L.marker([0,0]).addTo(map);
@@ -29,14 +34,28 @@ L.control.layers({
 
 // }
 
-console.log(CONFIRMED);
+let drawCircles=function(data) {
+   
+
+
+
+// console.log(CONFIRMED);
 
 // // for-Schleife über alle Arrays der CONFIRMED Einträge:
 
 for (let i = 1; i < CONFIRMED.length; i++) {
      let row = CONFIRMED[i];
      // console.log(row[2],row[3]);
+     let reg =`${row[0]} ${row[1]}`;
+     let lat = row [2];
+     let lng = row [3];
      let val = row[row.length-1];
-     let mrk = L.marker([row[2],row[3]]).addTo(map);
-     mrk.bindPopup(`${row[0]} ${row[1]}`);
-}
+    //  let mrk = L.marker([lat,lng]).addTo(map);
+    //  mrk.bindPopup(`${reg}: ${val}`);
+    let s = 0.5;
+    let r = Math.sqrt(val*s/Math.PI);
+    let circle = L.circleMarker([lat, lng]),{
+    radius: r
+}).addTo(map);
+circle.bindPopup(`${reg}: ${val}`);
+
