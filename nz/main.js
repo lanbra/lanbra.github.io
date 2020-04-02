74de0e02d986983a6a99e21d4516f358e59eb9c2
lanbra <laura.brandstetter@web.de>
2020-04-02 16:08:29 +0200

    andere art die karte zu machen

--- nz/main.js
+++ nz/main.js
@@ -9,20 +9,20 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap); */
 
-let Karte = document.querySelector("#map");
+// let Karte = document.querySelector("#map");
 
-let mymap = L.map(Karte).setView([-43.803056, 172.967778], 14);
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(mymap);
+// let mymap = L.map(Karte).setView([-43.803056, 172.967778], 14);
+// L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 17,
+//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+// }).addTo(mymap);
 
-let marker = L.marker([-43.803056, 172.967778]).addTo(mymap);
+// let marker = L.marker([-43.803056, 172.967778]).addTo(mymap);
 
-let popup = L.popup()
-    .setLatLng([-43.801056, 172.967778])
-    .setContent("Akaroa Harbour")
-    .openOn(mymap);
+// let popup = L.popup()
+//     .setLatLng([-43.801056, 172.967778])
+//     .setContent("Akaroa Harbour")
+//     .openOn(mymap);
 
 // let circle = L.circle([-43.803056, 172.967778], {
 //     color: 'red',
@@ -30,3 +30,22 @@ let popup = L.popup()
 //     fillOpacity: 0.3,
 //     radius: 200
 // }).addTo(mymap); 
+
+let mapdiv = document.querySelector("#map");
+let map = L.map("map", {
+    center: [
+        mapdiv.dataset.lat,
+        mapdiv.dataset.lng
+    ],
+    zoom: 13,
+    zoom: 17,
+    layers: [
+        L.tileLayer.provider("Stadia.AlidadeSmooth")
+        })
+    ]
+});
+let mrk = L.marker([
+    mapdiv.dataset.lat,
+    mapdiv.dataset.lng
+]).addTo(map);
+mrk.bindPopup(mapdiv.dataset.title).openPopup();
