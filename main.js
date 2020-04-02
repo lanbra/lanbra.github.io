45ea7142cc73eb53774025a1167157d6fe1a267f
lanbra <laura.brandstetter@web.de>
2020-04-02 18:25:36 +0200

    grampf

--- main.js
+++ main.js
@@ -42,20 +42,24 @@ let drawCircles=function(data) {
 // console.log(CONFIRMED);
 
 // // for-Schleife über alle Arrays der CONFIRMED Einträge:
+let drawCircles = function (data) {
+    for (let i = 1; i < data.length; i++) {
+        let row = data[i];
+        // console.log(row[2],row[3]);
+        let reg =`${row[0]} ${row[1]}`;
+        let lat = row [2];
+        let lng = row [3];
+        let val = row[row.length-1];
+        //  let mrk = L.marker([lat,lng]).addTo(map);
+        //  mrk.bindPopup(`${reg}: ${val}`);
+        let s = 0.5;
+        let r = Math.sqrt(val*s/Math.PI);
+        let circle = L.circleMarker([lat, lng],
+            radius: r
+        }).addTo(map);
 
-for (let i = 1; i < CONFIRMED.length; i++) {
-     let row = CONFIRMED[i];
-     // console.log(row[2],row[3]);
-     let reg =`${row[0]} ${row[1]}`;
-     let lat = row [2];
-     let lng = row [3];
-     let val = row[row.length-1];
-    //  let mrk = L.marker([lat,lng]).addTo(map);
-    //  mrk.bindPopup(`${reg}: ${val}`);
-    let s = 0.5;
-    let r = Math.sqrt(val*s/Math.PI);
-    let circle = L.circleMarker([lat, lng]),{
-    radius: r
-}).addTo(map);
-circle.bindPopup(`${reg}: ${val}`);
+        circle.bindPopup(`${reg}: ${val}`);
+        
+    }
+};
 
