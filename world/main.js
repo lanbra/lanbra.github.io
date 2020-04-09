26ca9a51430c3960f29a779631bcb77b0621f520
lanbra <laura.brandstetter@web.de>
2020-04-09 17:17:30 +0200

    alles neu kopiert

--- world/main.js
+++ world/main.js
@@ -1,6 +1,8 @@
-let startLayer = L.tileLayer.provider("OpenTopoMap")
+  
+let startLayer = L.tileLayer.provider("Esri.WorldTopoMap");
+
 let map = L.map("map", {
-    center: [0,0],
+    center: [30, 0],
     zoom: 2,
     layers: [
         startLayer
@@ -9,74 +11,61 @@ let map = L.map("map", {
 
 let circleGroup = L.featureGroup().addTo(map);
 
-
 L.control.layers({
-    "OpenTopoMap":startLayer,
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
-    "Esri.OceanBasemap": L.tileLayer.provider("Esri.OceanBasemap"),
+    "Stamen.TonerLite": L.tileLayer.provider("Stamen.TonerLite"),
     "Stamen.Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
-    "Stamen.Toner": L.tileLayer.provider("Stamen.Toner")
-    
+    "Stamen.Terrain": L.tileLayer.provider("Stamen.Terrain"),
+    "Stamen.TerrainBackground": L.tileLayer.provider("Stamen.TerrainBackground"),
+    "Esri.WorldStreetMap": L.tileLayer.provider("Esri.WorldStreetMap"),
+    "Esri.WorldTopoMap": startLayer,
+    "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+    "Esri.WorldPhysical": L.tileLayer.provider("Esri.WorldPhysical"),
+    "Esri.WorldGrayCanvas": L.tileLayer.provider("Esri.WorldGrayCanvas"),
+    "CartoDB.Positron": L.tileLayer.provider("CartoDB.Positron")
 }, {
-    "Thematische Darstellung":circleGroup
+    "Thematische Darstellung": circleGroup
 }).addTo(map);
 
-// L.marker([0,0]).addTo(map);
-
-// // console.log(CONFIRMED);
-// for (let i=1; i < CONFIRMED.length; i++) {
-//     let row = CONFIRMED [i];
-//     // console.log(row[2],row[3]);
-//     let val = row[row.length-1];
-//     let mrk = L.marker([row[2], row[3]]).addTo(map);
-//     mrk.bindPopup(`$(row[0]} $(row[1]}`);
-
-// }
-// console.log(CONFIRMED);
-
-// // for-Schleife über alle Arrays der CONFIRMED Einträge:
 let drawCircles = function () {
     let data = CONFIRMED;
     let header = CONFIRMED[0];
-    let index = header.length -1;
-    let topic ="bestätigte Fälle";
+    let index = document.querySelector("#slider").value;
     let options = document.querySelector("#pulldown").options;
-    console.log(options);
     let value = options[options.selectedIndex].value;
     let label = options[options.selectedIndex].text;
-    console.log(value, label,options)
-//Datum anzeigen
+    let color;
+    //console.log(value,label,options);
 
-if (value === "confirmed") {
-    data = CONFIRMED;
-    color = "#0074D9";
-} else if (value === "deaths") {
-    data = DEATHS;
-    color = "#B10DC9";
-} else {
-    data = RECOVERED;
-    color = "#2ECC40";
-}
+    if (value === "confirmed") {
+        data = CONFIRMED;
+        color = "#0074D9";
+    } else if (value === "deaths") {
+        data = DEATHS;
+        color = "#B10DC9";
+    } else {
+        data = RECOVERED;
+        color = "#2ECC40";
+    }
 
-console.log(CONFIRMED == RECOVERED);
+    console.log(CONFIRMED == RECOVERED);
 
-document.querySelector("#datum").innerHTML=`am ${header[index]} - ${label}`;
+    // Datum & Thema anzeigen anzeigen
+    document.querySelector("#datum").innerHTML = `am ${header[index]} - ${label}`;
 
-circleGroup.clearLayers()
+    circleGroup.clearLayers();
 
+    //console.log(data);
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
-        // console.log(row[2],row[3]);
-        let reg =`${row[0]} ${row[1]}`;
-        let lat = row [2];
-        let lng = row [3];
+        //console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
+        let lat = row[2];
+        let lng = row[3];
         let val = row[index];
-
-        if (val === "0") {
-            continue;
-        }
-
+        //let mrk = L.marker([lat,lng]).addTo(map);
+        //mrk.bindPopup(`${reg}: ${val}`);
 
         //A = r²*PI
         //r² = A/PI
@@ -97,12 +86,12 @@ document.querySelector("#pulldown").onchange = function() {
 
 let slider = document.querySelector("#slider");
 slider.min = 4;
-slider.max = CONFIRMED[0].length -1;
-slider.step=1;
-slider.value=slider.max;
+slider.max = CONFIRMED[0].length - 1;
+slider.step = 1;
+slider.value = slider.max;
 
 slider.onchange = function () {
     drawCircles();
-}
+};
 
-
+drawCircles()
