9ef70dbe32651dee54f8618316f1c61a8ea33baa
lanbra <laura.brandstetter@web.de>
2020-04-23 17:23:08 +0200

    color.js aus lawinen.report seite

---
+++ aws-tirol/colors.js
@@ -0,0 +1,48 @@
+
+Temperatur
+rgb(250,55,150)
+ fill="rgb(255,5,5)"
+ fill="rgb(255,85,54)
+rgb(255,154,53)
+rgb(255,190,130)
+rgb(255,255,103)
+rgb(176,255,188)
+rgb(140,255,255)
+rgb(3,205,255)
+rgb(4,129,255)
+rgb(3,91,190)
+rgb(120,75,255)
+rgb(159,128,255)
+<line class="legend-border" x1="0" x2="41" y1="0" y2="0"></line>
+<line class="legend-border" x1="0" x2="41" y1="234" y2="234"></line><text class="legend-text" x="20.5" y="18"
+    dy="0.35em" font-size="12" text-anchor="middle">30</text><text class="legend-text" x="20.5" y="36" dy="0.35em"
+    font-size="12" text-anchor="middle">25</text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12"
+    text-anchor="middle">20</text><text class="legend-text" x="20.5" y="72" dy="0.35em" font-size="12"
+    text-anchor="middle">15</text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12"
+    text-anchor="middle">10</text><text class="legend-text" x="20.5" y="108" dy="0.35em" font-size="12"
+    text-anchor="middle">5</text><text class="legend-text" x="20.5" y="126" dy="0.35em" font-size="12"
+    text-anchor="middle">0</text><text class="legend-text" x="20.5" y="144" dy="0.35em" font-size="12"
+    text-anchor="middle">-5</text><text class="legend-text" x="20.5" y="162" dy="0.35em" font-size="12"
+    text-anchor="middle">-10</text><text class="legend-text" x="20.5" y="180" dy="0.35em" font-size="12"
+    text-anchor="middle">-15</text><text class="legend-text" x="20.5" y="198" dy="0.35em" font-size="12"
+    text-anchor="middle">-20</text><text class="legend-text" x="20.5" y="216" dy="0.35em" font-size="12"
+    text-anchor="middle">-25</text>
+
+
+
+Wind
+<rect class="legend-rect" width="41" height="18" x="0" y="0" fill="rgb(255,50,50)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="18" fill="rgb(150,100,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="36" fill="rgb(100,150,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="54" fill="rgb(50,200,255)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="72" fill="rgb(150,255,150)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="90" fill="rgb(200,255,100)"></rect>
+<rect class="legend-rect" width="41" height="18" x="0" y="108" fill="rgb(255,255,100)"></rect>
+<line class="legend-border" x1="0" x2="41" y1="0" y2="0"></line>
+<line class="legend-border" x1="0" x2="41" y1="126" y2="126"></line><text class="legend-text" x="20.5" y="18"
+    dy="0.35em" font-size="12" text-anchor="middle">80</text><text class="legend-text" x="20.5" y="36" dy="0.35em"
+    font-size="12" text-anchor="middle">60</text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12"
+    text-anchor="middle">40</text><text class="legend-text" x="20.5" y="72" dy="0.35em" font-size="12"
+    text-anchor="middle">20</text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12"
+    text-anchor="middle">10</text><text class="legend-text" x="20.5" y="108" dy="0.35em" font-size="12"
+    text-anchor="middle">5</text>
