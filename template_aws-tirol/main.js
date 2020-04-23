977f8a9e25a6afc7beef016a2fab22cb541eea5b
lanbra <laura.brandstetter@web.de>
2020-04-23 12:32:50 +0200

    Link zu Wetterstation

--- template_aws-tirol/main.js
+++ template_aws-tirol/main.js
@@ -47,16 +47,16 @@ let aws = L.geoJson.ajax(awsUrl, {
         console.log("point: ", point);
         let marker = L.marker(latlng).bindPopup(`
         <h3>${point.properties.name}</h3>
-        <h3>${point.geometry.coordinates[2]} m</h3>
+        <h3>Höhe: ${point.geometry.coordinates[2]} m</h3>
         <ul>
-        <li>Position:  ${point.geometry.coordinates[0]} ${point.geometry.coordinates[1]}</li>s
+        <li>Position:  ${point.geometry.coordinates[0]} ${point.geometry.coordinates[1]}</li>
         <li>Datum: ${point.properties.date}</li>
         <li>Lufttemperatur: ${point.properties.LT} °C</li>
         <li>Windgeschwindigkeit:  ${point.properties.WG} m/s</li>
         <li> Relative Luftfeuchte:${point.properties.RH} %</li>
         <li> Schneehöhe: ${point.properties.HS} cm</li> 
-        </ul>
-        `);
+        <li> Grafik Wetterstation: <a href=https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png> ${point.properties.plot} </a></li>
+        </ul>`);
         return marker;
     }
 }).addTo(awsLayer);
