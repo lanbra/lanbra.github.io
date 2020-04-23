1683cf06718893a8ab45fc225a37b7d06f8b0024
lanbra <laura.brandstetter@web.de>
2020-04-23 16:21:01 +0200

    fehlersuche unmöglich

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -6,7 +6,7 @@ let map = L.map("map", {
     ]
 });
 
-let overlay = { 
+let overlay = {
     stations: L.featureGroup(),
     temperature: L.featureGroup()
 }
@@ -19,33 +19,34 @@ L.control.layers({
     "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
     "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
     "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
-    "BasemapAT.orthofoto+overlay": L.layerGroup([
+    "BasemapAT.orthofoto+overlay": L.layerGroup(
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
     ])
 }, {
-    "Wetterstationen Tirol": overlay
-}).addTo(map);
+"Wetterstationen Tirol": overlay.stations,
+"Temperatur (°C):": overlay.temperature
+}).addTo(map
 
-let awsUrl = "https://aws.openweb.cc/stations";
+        let awsUrl = "https://aws.openweb.cc/stations";
 
-let aws = L.geoJson.ajax(awsUrl, {
-    filter: function (feature) {
-        console.log("Feature in filter:", feature)
-        // if (feature.properties.LT < 5) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        // return feature.properties.LT < 5;
-        // return (feature.geometry.coordinates[2] > 3000)
-        return feature.properties.LT != null;
+        let aws = L.geoJson.ajax(awsUrl, {
+            filter: function (feature) {
+                console.log("Feature in filter:", feature)
+                // if (feature.properties.LT < 5) {
+                //     return true;
+                // } else {
+                //     return false;
+                // }
+                // return feature.properties.LT < 5;
+                // return (feature.geometry.coordinates[2] > 3000)
+                return feature.properties.LT != null;
 
 
-    },
-    pointToLayer: function (point, latlng) {
-        console.log("point: ", point);
-        let marker = L.marker(latlng).bindPopup(`
+            },
+            pointToLayer: function (point, latlng) {
+                console.log("point: ", point);
+                let marker = L.marker(latlng).bindPopup(`
         <h3>${point.properties.name}</h3>
         <h3>Höhe: ${point.geometry.coordinates[2]} m</h3>
         <ul>
@@ -57,25 +58,29 @@ let aws = L.geoJson.ajax(awsUrl, {
         <li> Schneehöhe: ${point.properties.HS} cm</li> 
         <li> Grafik Wetterstation: <a href=https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png> ${point.properties.plot} </a></li>
         </ul>`);
-        return marker;
-    }
-}).addTo(overlay.stations);
+                return marker;
+            }
+        }).addTo(overlay.stations);
 
-let drawTemperature = function(jsonData) {
-    console.log("aus der Funktion", jsonData);
-    L.geoJson(jsonData, {
-        pointToLayer:function(feature, latlng) {
-            return L.marker(latlng {
-                title: `${feature.properties.name}(`${feature.geomety.coordinates[2]}`)`
-            })
-        }
-    }).addTo(overlay.temperature);
-};
+        let drawTemperature = function (jsonData) {
+            console.log("aus der Funktion", jsonData);
+            L.geoJson(jsonData, {
+                pointToLayer: function (feature, latlng) {
+                    return L.marker(latlng, {
+                        title: `${feature.properties.name} (${feature.geomety.coordinates[2]}m)`,
+                        icon: L.divIcon({
+                            html: `<div>${feature.properties.LT.toFixed(1)} </div>`,
+                            className: "ignore-me"
+                        })
+                    })
+                }
+            }).addTo(overlay.temperature);
+        };
 
-aws.on("data:loaded", function() {
-    console.log(aws.toGeoJson)
-    drawTemperature(aws.toGeoJson)
-    map.fitBounds(overlay.stations.getBounds());
+        aws.on("data:loaded", function () {
+            console.log(aws.toGeoJson)
+            drawTemperature(aws.toGeoJson)
+            map.fitBounds(overlay.stations.getBounds());
 
-    overlay.temperature.addTo(map)
-});
+            overlay.temperature.addTo(map)
+        });
